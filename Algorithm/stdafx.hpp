8e88f75e753bff748e3cbdaf22c785f8c1a894d9
estKey <estkey@outlook.com>
2018-11-09 21:19:33 -0500

    update1

---
+++ Algorithm/stdafx.hpp
@@ -0,0 +1,12 @@
+///Author: estKey
+///if you are watching this file fuck off if you dont understand learn Chinese
+
+#pragma once
+#ifndef STDAFX_HPP_INCLUDED
+#define STDAFX_HPP_INCLUDED
+
+
+#include "SortingAlgorithm.hpp"
+
+#endif // !STDAFX_HPP_INCLUDED
+
